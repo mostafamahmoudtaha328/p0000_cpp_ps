a4146b14375c60a01924515b628644cd5feb9847
mostafamahmoudtaha328 <162768361+mostafamahmoudtaha328@users.noreply.github.com>
2024-08-21 12:44:53 +0300

    Add files via upload

---
+++ pc0000.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // is # Not @ ||| is <iostream> Not <"iostram">
+
+int main() // all letter is small
+{
+  std:: cout << "Hello, World!\n"; //<< for show ||| >> for read  ||| \n not /n ||| ; in end ||| std:: 
+  return 0; // 0 Not 1 or any number only 0
+}
