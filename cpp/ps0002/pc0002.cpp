536f97f5e9cc0c914d2e7d13fe951a0c46db71f5
mostafamahmoudtaha328 <162768361+mostafamahmoudtaha328@users.noreply.github.com>
2024-08-21 17:48:24 +0300

    Add files via upload

---
+++ cpp/ps0002/pc0002.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    short num_of_pepole = 52;
+    short num_of_medicine = 10;
+    float num_of_price = 2.6;
+    char first_letter_of_name = 'M';
+    long long num_of_members = 100200000;
+    double num_of_mod7 = 15 / 7;
+    float num_of_price_electronic = 456.33;
+    long long num_of_age = 1230000000;
+    
+    cout << num_of_pepole << endl;
+    cout << num_of_medicine << endl;
+    cout << num_of_price << endl;
+    cout << first_letter_of_name << endl;
+    cout << num_of_members << endl;
+    cout << num_of_mod7 << endl;
+    cout << num_of_price_electronic << endl;
+    cout << num_of_age << endl;
+
+    return 0;
+}
