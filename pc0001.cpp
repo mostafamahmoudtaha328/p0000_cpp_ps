3acee05543f9624844f28ceda512c96bf05ba2df
mostafamahmoudtaha328 <162768361+mostafamahmoudtaha328@users.noreply.github.com>
2024-08-21 13:16:52 +0300

    Add files via upload

---
+++ pc0001.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1 , num2 , num3;
+    cout << "Enter Three numbers: ";
+    cin >> num1 >> num2 >> num3;
+    cout << num1 + num2 + num3 << endl;
+    return 0;
+}
