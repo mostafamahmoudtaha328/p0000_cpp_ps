08f81125f744b648aa7c0589de03461e5ec5055b
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-09-10 18:37:25 +0300

    problem 5 marked

--- cpp/ps0005/pc0005.cpp
+++ cpp/ps0005/pc0005.cpp
@@ -3,12 +3,16 @@
 using namespace std;
 int main()
 {
+    // prompting user for 2 operands and an operator 2/2
     float num1 , num2;
     cout << "Enter tow operands: ";
     cin >> num1 >> num2;
     char op;
     cout << "Enter the operator + - * / % ";
     cin >> op;
+
+    // checking user inputs and processing 4/4
+    // printing the result if all data is valid 1/1
     if(num2 == 0)
     {
         if (num1 == 0 && (op == '/' || op == '%')) cout << "Infinite number of solutions: (0 / 0) is Undefined Quantity.";
@@ -63,3 +67,4 @@ int main()
     }
     return 0;
 }
+// great work 😎 8.5/10 ⭐
